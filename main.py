15474eac67d1a9a3f1541f71a2b229989a7d77b7
hrlaan12 <ujang.herlan_ti22@nusaputra.ac.id>
2022-10-11 10:12:09 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print ("============TYPE DATA=============")
+interger = 33400
+string = "Eko Wahyudin"
+flo = 1.3
+bol = False
+
+print ("type data adalah",(type(interger)))
+print ("type data adalah",(type(string)))
+print ("type data adalah",(type(flo)))
+print ("type data adalah",(type(bol)))
+print ("==================================")
+
+print ("==============DATA DIRi============")
+nama = "Eko Wahyudin Nur Racan"
+nim = "20220040079"
+no_hp = "085721375082"
+print (nama)
+print (nim)
+print (no_hp)
